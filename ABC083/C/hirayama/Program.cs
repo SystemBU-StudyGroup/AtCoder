e81063ecc8ec5519db9d5a5f964d581ea92200a6
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-06-20 12:06:55 +0900

    [add]ABC083 C

---
+++ ABC083/C/hirayama/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace agc083_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+            long x = inputLongArray[0];
+            long y = inputLongArray[1];
+            long cnt = 0;
+            while (x <= y)
+            {
+                x *= 2;
+                cnt++;
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
